b281f93bd82de69b30184532bedf393797d72dd0
unknown <k.jones45@yahoo.com>
2023-07-11 19:19:57 -0400

    Installed bootstrap for frontend styling. + added fields to exercises models.py

--- backend/excercises/models.py
+++ backend/excercises/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
+
+class Exercise(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sqauts = models.IntegerField()
+    lunges = models.IntegerField()
+    dead_lifts = models.IntegerField()
+    shoulder_press = models.IntegerField()
+    bicep_curls = models.IntegerField()
+    tricep_kickbacks = models.IntegerField()
